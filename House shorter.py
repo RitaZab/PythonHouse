8cafec093aca4b623347b788fb50869035f3772d
RitaZab <98622430+RitaZab@users.noreply.github.com>
2022-04-18 14:42:32 +0200

    Add files via upload

--- House shorter.py
+++ House shorter.py
@@ -44,20 +44,35 @@ def turtle_rounding():
     turtle.forward(0.2)
     turtle.right(1)
 
+def turtle_rounding_big():
+     turtle.forward(1)
+     turtle.left(1)
+
 def turtle_fence():
     turtle.forward(15)
     turtle.left(90)
     turtle.forward(80)
     turtle.left(45)
-    turtle.forward(10)
+    turtle.forward(10.5)
     turtle.left(90)
-    turtle.forward(10)
+    turtle.forward(10.5)
     turtle.left(45)
     turtle.forward(80)
     turtle.left(90)
     turtle.forward(15)
 
+def turtle_rounding_big():
+    turtle.forward(1)
+    turtle.left(1)
 
+def turtle_sun():
+    for q in range(10):
+        turtle_rounding_big()
+    turtle.right(45)
+    turtle.forward(30)
+    turtle.left(120)
+    turtle.forward(30)
+    turtle.right(45)
 
 
 turtle_square()
@@ -97,16 +112,31 @@ turtle.left(90)
 turtle.color('white')
 turtle.forward(17)
 turtle.color('black')
+turtle.speed('fastest')
 for x in range(360):
     turtle_rounding()
+turtle.forward(3)
 turtle.color('white')
-turtle.forward(18)
+turtle.speed('slowest')
+turtle.forward(15)
 turtle.color('black')
 turtle.right(90)
 turtle.forward(30)
 turtle.left(90)
 turtle.forward(150)
-turtle_fence()
+for y in range(5):
+    turtle_fence()
+turtle.color('white')
+turtle.forward(10)
+turtle.left(90)
+turtle.forward(300)
+turtle.color('orange')
+for t in range(9):
+    turtle_sun()
+turtle.speed('fastest')
+for x in range(360):
+    turtle_rounding_big()
+
 
 
 
